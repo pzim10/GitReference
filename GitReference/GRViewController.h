149792e1307be37f5e313ec870b283d2a3bd35a7
Peter Zimmerman <pzim10@gmail.com>
2015-06-23 21:03:00 -0600

    Initial Code

---
+++ GitReference/GRViewController.h
@@ -0,0 +1,13 @@
+//
+//  GRViewController.h
+//  GitReference
+//
+//  Created by Peter Zimmerman on 6/23/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GRViewController : UIViewController
+
+@end
---
+++ GitReference/GRViewController.m
@@ -0,0 +1,13 @@
+//
+//  GRViewController.m
+//  GitReference
+//
+//  Created by Peter Zimmerman on 6/23/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import "GRViewController.h"
+
+@implementation GRViewController
+
+@end
